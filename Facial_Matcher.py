46ba9e4c3ea579a807a37ec79244b1cce89f1a17
sam-spears <73221692+sam-spears@users.noreply.github.com>
2021-04-16 15:19:55 +0100

    Add files via upload

--- Facial_Matcher.py
+++ Facial_Matcher.py
@@ -7,7 +7,7 @@ template = cv2.imread("Resources/Picture.jpg")
 head_out = cv2.imread("Resources/head_outline.jpg")
 
 # percent by which the image is resized
-scale_percent = 30
+scale_percent = 20
 # calculate the percentage of original dimensions
 width = int(imgRaw.shape[1] * scale_percent / 100)
 height = int(imgRaw.shape[0] * scale_percent / 100)
@@ -17,7 +17,7 @@ imgAdj = cv2.resize(imgRaw, dsize)
 
 imgGrey = cv2.cvtColor(imgRaw, cv2.COLOR_BGR2GRAY)
 imgBlur = cv2.GaussianBlur(imgGrey, (5, 5), 0)
-imgCanny = cv2.Canny(imgBlur, 150, 150)
+imgCanny = cv2.Canny(imgBlur, 50, 150)
 invCanny = cv2.bitwise_not(imgCanny)
 
 templateFlip = cv2.flip(template, 1)
@@ -77,14 +77,16 @@ def getTemplateKpts(event, x, y, flags, params):
 
 face_cascade = cv2.CascadeClassifier("Resources/haarcascade_profileface.xml")
 
-faces = face_cascade.detectMultiScale(imgRaw, 1.1, 5)
+faces = face_cascade.detectMultiScale(imgGrey, 1.1, 5)
+
+roi = []
 
 for (x, y, w, h) in faces:
-    cv2.rectangle(invCanny, (x, y), (x + w, y + h), (255, 255, 255), 2)
+    cv2.rectangle(imgGrey, (x, y), (x + w, y + h), (255, 255, 255), 2)
     print(x, y, w, h)
-    roi = imgGrey[y - 200:y + (h + 200), x:x + (w + 300)]
+    roi = invCanny[y - 200:y + (h + 200), x:x + (w + 300)]
 
-def alignImages(image, template, MaxFeatures=50, Good_Match_Perc=0.95, debug=True):
+def alignImages(image, template, debug=True):
 
     image = image
     template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
@@ -117,45 +119,67 @@ def alignImages(image, template, MaxFeatures=50, Good_Match_Perc=0.95, debug=Tru
 
     #turn points into keypoints
 
-    kpts1 = [cv2.KeyPoint(imgpts[0][0], imgpts[0][1], 1), cv2.KeyPoint(imgpts[1][0], imgpts[1][1], 1), cv2.KeyPoint(imgpts[2][0], imgpts[2][1], 1),
+    pts1 = [cv2.KeyPoint(imgpts[0][0], imgpts[0][1], 1), cv2.KeyPoint(imgpts[1][0], imgpts[1][1], 1), cv2.KeyPoint(imgpts[2][0], imgpts[2][1], 1),
              cv2.KeyPoint(imgpts[3][0], imgpts[3][1], 1), cv2.KeyPoint(imgpts[4][0], imgpts[4][1], 1), cv2.KeyPoint(imgpts[5][0], imgpts[5][1], 1)]
 
-    kpts2 = [cv2.KeyPoint(tempts[0][0], tempts[0][1], 1), cv2.KeyPoint(tempts[1][0], tempts[1][1], 1), cv2.KeyPoint(tempts[2][0], tempts[2][1], 1),
+    pts2 = [cv2.KeyPoint(tempts[0][0], tempts[0][1], 1), cv2.KeyPoint(tempts[1][0], tempts[1][1], 1), cv2.KeyPoint(tempts[2][0], tempts[2][1], 1),
              cv2.KeyPoint(tempts[3][0], tempts[3][1], 1), cv2.KeyPoint(tempts[4][0], tempts[4][1], 1), cv2.KeyPoint(tempts[5][0], tempts[5][1], 1)]
 
     orb = cv2.ORB_create(6)
-    (keypoints1, descriptors1) = orb.compute(image, kpts1)
-    (keypoints2, descriptors2) = orb.compute(template, kpts2)
+    (kpts1, des1) = orb.compute(image, pts1)
+    (kpts2, des2) = orb.compute(template, pts2)
 
-    matcher = cv2.DescriptorMatcher_create(cv2.DESCRIPTOR_MATCHER_BRUTEFORCE_HAMMING)
-    matches = matcher.match(descriptors1, descriptors2, None)
-
+    matcher = cv2.BFMatcher()
+    matches = matcher.match(des1, des2, None)
     matches = sorted(matches, key=lambda x: x.distance)
 
-    numGoodMatches = int(len(matches) * Good_Match_Perc)
-    matches = matches[:numGoodMatches]
-
     if debug:
-        matchedVis = cv2.drawMatches(image, keypoints1, template, keypoints2,
-                                     matches, None)
+        matchedVis = cv2.drawMatches(image, kpts1, template, kpts2, matches, outImg=None)
         matchedVis = imutils.resize(matchedVis, width=1000)
         cv2.imshow("Matched Keypoints", matchedVis)
         cv2.imwrite("Aligned.jpg", matchedVis)
         cv2.waitKey(0)
 
+    # # Transformation matrix, T
+    # T_s = np.array([[2, 0, 0], [0, 2, 0], [0, 0, 1]])  # Scaling matrix
+    # T_r = np.array([[0, 1, 0], [-1, 0, 0], [0, 0, 1]])  # Rotational matrix
+    # T = T_s @ T_r
+    #
+    # Transform = np.float32(T.flatten()[:6].reshape(2, 3))
+    # img_transformed = cv2.warpAffine(image, Transform, (100, 100))
+    # cv2.imshow("transformed", img_transformed)
+    # cv2.waitKey(0)
+    #
     ptsA = np.zeros((len(matches), 2), dtype="float")
     ptsB = np.zeros((len(matches), 2), dtype="float")
 
+    list_kpts1 = []
+    list_kpts2 = []
+
     # loop over the top matches
-    for i, m in enumerate(matches):
-        # indicate that the two keypoints in the respective images
-        # map to each other
-        ptsA[i] = keypoints1[m.queryIdx].pt
-        ptsB[i] = keypoints2[m.trainIdx].pt
+    for m in matches:
+
+        # Get the matching keypoints for each of the images
+        img1_idx = m.queryIdx
+        img2_idx = m.trainIdx
+
+        # x - columns
+        # y - rows
+        # Get the coordinates
+        (x1, y1) = kpts1[img1_idx].pt
+        (x2, y2) = kpts2[img2_idx].pt
+
+        # Append to each list
+        list_kpts1.append((x1, y1))
+        list_kpts2.append((x2, y2))
+
+        list_kpts1 = [kpts1[m.queryIdx].pt for m in matches]
+        list_kpts2 = [kpts2[m.trainIdx].pt for m in matches]
+
 
     # compute the homography matrix between the two sets of matched
     # points
-    (H, mask) = cv2.findHomography(ptsA, ptsB, method=cv2.RANSAC)
+    (H, mask) = cv2.findHomography(list_kpts1, list_kpts2, method=cv2.RANSAC)
 
     # use the homography matrix to align the images
     (h, w) = template.shape[:2]
@@ -164,17 +188,53 @@ def alignImages(image, template, MaxFeatures=50, Good_Match_Perc=0.95, debug=Tru
     # return the aligned image
     return aligned
 
+def overlay_transparent(background, overlay, x, y):
 
-image = roi.copy()
+    background_width = background.shape[1]
+    background_height = background.shape[0]
+
+    if x >= background_width or y >= background_height:
+        return background
+
+    h, w = overlay.shape[0], overlay.shape[1]
+
+    if x + w > background_width:
+        w = background_width - x
+        overlay = overlay[:, :w]
+
+    if y + h > background_height:
+        h = background_height - y
+        overlay = overlay[:h]
+
+    if overlay.shape[2] < 4:
+        overlay = np.concatenate(
+            [
+                overlay,
+                np.ones((overlay.shape[0], overlay.shape[1], 1), dtype = overlay.dtype) * 255
+            ],
+            axis = 2,
+        )
+
+    overlay_image = overlay[..., :3]
+    mask = overlay[..., 3:] / 255.0
+
+    background[y:y+h, x:x+w] = (1.0 - mask) * background[y:y+h, x:x+w] + mask * overlay_image
+
+    return background
+
+
+image = roi
 template = templateFlip
 
-align = alignImages(image, templateFlip, debug=True)
 
-image_alg = imutils.resize(align, width=500)
-template_alg = imutils.resize(template, width=500)
 
-overlay = template_alg
-output = image_alg
-
-cv2.imshow("overlay", align)
-cv2.waitKey(0)
+align = alignImages(image, template, debug=True)
+#
+# image_alg = imutils.resize(align, width=500)
+# template_alg = imutils.resize(template, width=500)
+#
+# overlay = template_alg
+# output = image_alg
+#
+# cv2.imshow("overlay", align)
+# cv2.waitKey(0)
